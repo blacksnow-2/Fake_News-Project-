2b15650bb2c8f4c70d066ba8d0bcf8fccba8dff7
Ishan Mandal <ishanmandalktpp@gmail.com>
2024-04-08 01:56:53 +0530

    Modified Features.py, tree_based.py, and other subfiles

---
+++ fake_news/model/tree_based.py
@@ -0,0 +1,95 @@
+import logging
+import pickle
+import os
+from typing import Dict
+from typing import List
+from typing import Optional
+
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+# Precision = TP/ (TP+FP). Recall = TP(TP+FN). f1 score is harmonic mean of Precision and recall
+# f1 = 2 * (1 / (1/Precision + 1/Recall))
+from sklearn.metrics import f1_score
+# Below for roc_auc_score
+# https://www.evidentlyai.com/classification-metrics/explain-roc-curve
+from sklearn.metrics import roc_auc_score
+
+from fake_news.model.base import Model
+from fake_news.utils.features import Datapoint
+from fake_news.utils.features import TreeFeaturizer
+
+logging.basicConfig(
+    format="%(levelname)s - %(asctime)s - %(filename)s - %(message)s",
+    level=logging.DEBUG
+)
+LOGGER = logging.getLogger(__name__)
+
+class RandomForestModel(Model):
+    def __init__(self, config: Optional[Dict] = None):
+        self.config = config
+        model_cache_path = os.path.join(config["model_output_path"], "model.pkl")
+        self.featurizer = TreeFeaturizer(os.path.join(config["featurizer_output_path"], "featurizer.pkl"), config)
+        if "evaluate" in config and config["evaluate"] and not os.path.exists(model_cache_path):
+            raise ValueError("Model output path does not exist but in `evaluate` model!")
+        if model_cache_path and os.path.exists(model_cache_path):
+            LOGGER.info("Loading model from cache...")
+            with open(model_cache_path, "rb") as f:
+                self.model = pkl.load(f)
+        else:
+            LOGGER.info("Initializing model from scratch...")
+            self.model = RandomForestClassifier(**self.config["params"])
+
+    def train(self,
+              train_datapoints: List[Datapoint],
+              val_datapoints: List[Datapoint] = None,
+              cache_featurizer: Optionsl[bool] = False) -> None:
+        self.featurizer.fit(train_datapoints)
+        if cache_featurizer:
+            feature_names = self.featurizer.get_all_feature_names()
+            with open(os.path.join(self.config["mode_output_path"],
+                                    "feature_names.pkl"), "wb") as f:
+                pickle.dump(feature_names, f)
+            self.featurizer.save(os.path.join(self.config["featurizer_output_path"],
+                                              "featurizer.pkl"))
+        train_labels = [datapoint.label for datapoint in train_datapoints]
+        LOGGER.info("Featurizing data from scratch...")
+        train_features = self.featurizer.featurize(train_datapoints)
+        self.model.fit(train_features, train_labels)
+
+    def compute_metrics(self, eval_datapoints: List[Datapoint], split: Optional[str] = None) -> Dict:
+        expected_labels = [datapoint.label for datapoint in eval_datapoints]
+        predicted_proba = self.predict(eval_datapoints)
+        predicted_labels = np.argmax(predicted_proba, axis=1)
+        accuracy = accuracy_score(expected_labels, predicted_labels)
+        f1 = f1_score(expected_labels, predicted_labels)
+        # ??
+        auc = roc_auc_score(expected_labels, predict_proba[:, 1])
+        conf_mat = confusion_matrix(expected_labels, predicted_labels)
+        tn, fp, fn, tp = conf_mat.ravel()
+        split_prefix = "" if split is None else split
+        return {
+            f"{split_prefix} f1": f1,
+            f"{split_prefix} accuracy": accuracy,
+            f"{split_prefix} auc": auc,
+            f"{split_prefix} true negative": tn,
+            f"{split_prefix} false negative": fn,
+            f"{split_prefix} false positive": fp,
+            f"{split_prefix} true positive": tp,
+        }
+
+    def predict(self, datapoints: List[Datapoint]) -> np.array:
+        # what is features??
+        features = self.featurizer.featurize(datapoints)
+        return self.model.predict_proba(features)
+    
+    def get_params(self) -> Dict:
+        return self.model.get_params()
+
+    def save(self, model_cache_path: str) -> None:
+        LOGGER.info("Saving model to disk...")
+        with open(model_cache_path, "wb") as f:
+            pickle.dump(self.model, f)
+
+
--- fake_news/utils/features.py
+++ fake_news/utils/features.py
@@ -28,7 +28,7 @@ logging.basicConfig(
 LOGGER = logging.getLogger(__name__)
 
 
-class Datapoint(Basemodel):
+class Datapoint(BaseModel):
     id: Optional[str]
     statement_json: Optional[str]
     label: Optional[bool]
@@ -46,6 +46,122 @@ class Datapoint(Basemodel):
     context: Optional[str]
     justification: Optional[str]
 
+
+def extract_manual_features(datapoints: List[Datapoint], optimal_credit_bins: Dict) -> List[Dict]:
+    all_features = []
+    for datapoint in datapoints:
+        features = {}
+        features["speaker"] = datapoint.speaker
+        features["speaker_title"] = datapoint.speaker_title
+        features["state_info"] = datapoint.state_info
+        features["party_affiliation"] = datapoint.party_affiliation
+        # Compute credit score features?
+        datapoint = dict(datapoint)
+        for feat in ["barely_true_count", "false_count", "half_true_count", "mostly_true_count", "pants_fire_count"]:
+            features[feat] = str(compute_bin_idx(datapoint[feat], optimal_credit_bins[feat]))
+        all_features.append(features)
+    return all_features
+
+
+def extract_statements(datapoints: List[Datapoint]) -> List[str]:
+    return [datapoint.statement for datapoint in datapoints]
+
+
+def construct_datapoint(input: str) -> Datapoint:
+    return Datapoint(**{
+        "statement": input,
+        "barely_true_count": float("nan"),
+        "false_count": float("nan"),
+        "half_true_count": float("nan"),
+        "mostly_true_count": float("nan"),
+        "pants_fire_count": float("nan"),
+    })
+
+
+class TreeFeaturizer(object):
+    def __init__(self, featurizer_cache_path: str, config: Optional[Dict] = None):
+        # NOTE: Here you can add feature caching which helps if it's too expensive
+        # to compute features from scratch for each run
+        if os.path.exists(featurizer_cache_path):
+            LOGGER.info("Loading featurizer from cache...")
+            # r: 
+            # This mode opens the file for reading in text mode. Text mode is the default mode for opening files.
+            # When you open a file in text mode, Python will read the file as a sequence of strings, and certain newline characters might be translated to \n in Python. 
+            # rb:
+            # This mode opens the file for reading in binary mode.
+            # When you open a file in binary mode, Python will read the file as a sequence of bytes. No translation of newline characters or encoding/decoding is performed.
+            # This mode is typically used when you're dealing with non-text files, such as images, audio files, or binary data.
+            with open(featurizer_cache_path, "rb") as f:
+                self.combined_featurizer = pickle.load(f)
+        else:
+            LOGGER.info("Creating featurizer from scratch...")
+            # path.dirname reutrns the folder the current file/folder is present. Using it multiple times helps
+            # to go up the directory level given a path. Like applying it twice on a\b\c\d returns a\b.
+            base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+            # Load optimal credit bins?
+            with open(os.path.join(base_dir, config["credit_bins_path"])) as f:
+                optimal_credit_bins = json.load(f)
+            dict_featurizer = DictVectorizer()
+            tfidf_featurizer = TfidfVectorizer()
+
+            statement_transformer = FunctionTransformer(extract_statements)
+            manual_feature_transformer = FunctionTransformer(partial(extract_manual_features,
+                                                                     optimal_credit_bins=optimal_credit_bins))
+            
+            # Learn about Pipelines from sklearn API. Mainly
+            # Pipeline allows you to sequentially apply a list of transformers to preprocess the data and, if desired, 
+            # conclude the sequence with a final predictor for predictive modeling.
+            # The purpose of the pipeline is to assemble several steps that can be cross-validated together while setting 
+            # different parameters. For this, it enables setting parameters of the various steps using their names and the
+            # parameter name separated by a '__'. Parameter setting is here
+            # https://scikit-learn.org/stable/modules/generated/sklearn.pipeline.Pipeline.html#sklearn.pipeline.Pipeline
+            manual_feature_pipeline = Pipeline([
+                ("manual_features", manual_feature_transformer),
+                ("manual_featurizer", dict_featurizer)
+            ])
+
+            ngram_feature_pipeline = Pipeline([
+                ("statements", statement_transformer),
+                ("ngram_featurizer", tfidf_featurizer)
+            ])
+
+            # Feature Union Concatenates results of multiple transformer objects.
+            # This estimator applies a list of transformer objects in parallel to the input data, then concatenates
+            # the results. This is useful to combine several feature extraction mechanisms into a single transformer.
+            # More https://scikit-learn.org/stable/modules/generated/sklearn.pipeline.FeatureUnion.html#sklearn.pipeline.FeatureUnion
+            self.combined_featurizer = FeatureUnion([
+                ("manual_feature_pipe", manual_feature_pipeline),
+                ("ngram_feature_pipe", ngram_feature_pipeline)
+            ])
+    
+    def get_all_feature_names(self) -> List[str]:
+        all_feature_names = []
+        for name, pipeline in self.combined_featurizer.transformer_list:
+            # Extract the last transformer in each pipeline like dict_featurizer for thr first one
+            final_pipe_name, final_pipe_transformer = pipeline.steps[-1]
+            all_feature_names.extend(final_pipe_transformer.get_feature_names())
+        return all_feature_names
+    
+    def fit(self, datapoints: List[Datapoint]) -> None:
+        self.combined_featurizer.fit(datapoints)
+    
+    def featurize(self, datapoints: List[Datapoint]) -> np.array:
+        return self.combined_featurizer.transform(datapoints)
+    
+    def save(self, featurizer_cache_path: str):
+        LOGGER.info("Saving featurizer to disk...")
+        with open(featurizer_cache_path, "wb") as f:
+            pickle.dump(self.combined_featurizer, f)
+
+
+def compute_bin_idx(val: float, bins: List[float]) -> int:
+    for idx, bin_val in enumerate(bins):
+        if val <= bin_val:
+            return idx
+
+
+
+
 def normalize_labels(datapoints: List[Dict]) -> List[Dict]:
     normalized_datapoints = []
     for datapoint in datapoints:
@@ -54,6 +170,7 @@ def normalize_labels(datapoints: List[Dict]) -> List[Dict]:
         normalized_datapoint["label"] = SIX_WAY_LABEL_TO_BINARY[datapoint["label"].lower().strip()]
         normalized_datapoints.append(normalized_datapoint)
     return normalized_datapoints
+
 
 def normalize_and_clean_counts(datapoints: List[Dict]) -> List[Dict]:
     normalized_datapoints = []
@@ -93,6 +210,21 @@ def normalize_and_clean_party_affiliations(datapoints: List[Dict]) -> List[Dict]
             normalized_datapoint["party_affiliation"] = "none"
         normalized_datapoints.append(normalized_datapoint)
     return normalized_datapoints
+
+
+def normalize_and_clean_speaker_title(datapoints: List[Dict]) -> List[Dict]:
+    normalized_datapoints = []
+    for datapoint in datapoints:
+        # First do simple cleaning
+        normalized_datapoint = deepcopy(datapoint)
+        old_speaker_title = normalized_datapoint["speaker_title"]
+        old_speaker_title = old_speaker_title.lower().strip().replace("-", " ")
+        if old_speaker_title in CANONICAL_SPEAKER_TITLES:
+            old_speaker_title = CANONICAL_SPEAKER_TITLES[old_speaker_title]
+        normalized_datapoint["speaker_title"] = old_speaker_title
+        normalized_datapoints.append(normalized_datapoint)
+    return normalized_datapoints
+
 
 
 def normalize_and_clean(datapoints: List[Dict]) -> List[Dict]:
